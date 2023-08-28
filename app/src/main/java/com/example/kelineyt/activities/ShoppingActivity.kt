1594e34d7b1e9767cbbbddc7d66acdcd2ec7d5b4
Franky <frank.jr.619@gmail.com>
2023-08-28 10:16:36 +0700

    init project

---
+++ app/src/main/java/com/example/kelineyt/activities/ShoppingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kelineyt.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kelineyt.R
+
+class ShoppingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
