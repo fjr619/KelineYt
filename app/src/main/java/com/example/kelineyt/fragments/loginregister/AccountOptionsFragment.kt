5438d40da2d66bc931c8770eedc39424dffb17d0
Franky <frank.jr.619@gmail.com>
2023-08-28 10:49:24 +0700

    introduction fragment layout

---
+++ app/src/main/java/com/example/kelineyt/fragments/loginregister/AccountOptionsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.kelineyt.fragments.loginregister
+
+import androidx.fragment.app.Fragment
+import com.example.kelineyt.R
+
+class AccountOptionsFragment: Fragment(R.layout.fragment_account_options) {
+}
---
+++ app/src/main/java/com/example/kelineyt/fragments/loginregister/IntroductionFragment.kt
@@ -0,0 +1,7 @@
+package com.example.kelineyt.fragments.loginregister
+
+import androidx.fragment.app.Fragment
+import com.example.kelineyt.R
+
+class IntroductionFragment: Fragment(R.layout.fragment_introduction) {
+}
