764c5560e48809472b38847a2a20488172d6b86d
Franky <frank.jr.619@gmail.com>
2023-09-05 18:51:50 +0700

    tab layout

--- app/src/main/java/com/example/kelineyt/fragments/shopping/HomeFragment.kt
+++ app/src/main/java/com/example/kelineyt/fragments/shopping/HomeFragment.kt
@@ -1,7 +1,58 @@
 package com.example.kelineyt.fragments.shopping
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.kelineyt.R
+import com.example.kelineyt.adapters.HomeViewpagerAdapter
+import com.example.kelineyt.databinding.FragmentHomeBinding
+import com.example.kelineyt.fragments.categories.AccessoryFragment
+import com.example.kelineyt.fragments.categories.ChairFragment
+import com.example.kelineyt.fragments.categories.CupboardFragment
+import com.example.kelineyt.fragments.categories.FurnitureFragment
+import com.example.kelineyt.fragments.categories.MainCategoryFragment
+import com.example.kelineyt.fragments.categories.TableFragment
+import com.google.android.material.tabs.TabLayoutMediator
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeFragment: Fragment(R.layout.fragment_home) {
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val categoriesFragments = arrayListOf<Fragment>(
+            MainCategoryFragment(),
+            ChairFragment(),
+            CupboardFragment(),
+            TableFragment(),
+            AccessoryFragment(),
+            FurnitureFragment()
+        )
+
+        val viewPager2Adapter = HomeViewpagerAdapter(categoriesFragments, childFragmentManager, lifecycle)
+        binding.viewpagerHome.adapter = viewPager2Adapter
+        TabLayoutMediator(binding.tabLayout, binding.viewpagerHome) { tab, position ->
+            tab.text = when(position) {
+                0 -> "Main"
+                1 -> "Chair"
+                2 -> "Cupboard"
+                3 -> "Table"
+                4 -> "Accessory"
+                5 -> "Furniture"
+                else -> ""
+            }
+        }.attach()
+    }
 }
