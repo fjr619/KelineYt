26b521a8430be8d26a47036dc6d9287510275a10
Franky <frank.jr.619@gmail.com>
2023-09-05 18:26:53 +0700

    bottom navigation

--- app/src/main/java/com/example/kelineyt/activities/ShoppingActivity.kt
+++ app/src/main/java/com/example/kelineyt/activities/ShoppingActivity.kt
@@ -2,13 +2,24 @@ package com.example.kelineyt.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.kelineyt.R
+import com.example.kelineyt.databinding.ActivityShoppingBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ShoppingActivity : AppCompatActivity() {
+    val binding by lazy {
+        ActivityShoppingBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shopping)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.shoppingHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
