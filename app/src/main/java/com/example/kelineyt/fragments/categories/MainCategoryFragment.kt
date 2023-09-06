c9795795ff31f0907798c2edd62856887d0aa8f5
Franky <frank.jr.619@gmail.com>
2023-09-06 12:18:46 +0700

    special products

--- app/src/main/java/com/example/kelineyt/fragments/categories/MainCategoryFragment.kt
+++ app/src/main/java/com/example/kelineyt/fragments/categories/MainCategoryFragment.kt
@@ -1,7 +1,85 @@
 package com.example.kelineyt.fragments.categories
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kelineyt.R
+import com.example.kelineyt.adapters.SpecialProductsAdapter
+import com.example.kelineyt.databinding.FragmentMainCategoryBinding
+import com.example.kelineyt.util.Resource
+import com.example.kelineyt.viewmodel.MainCategoryViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
-class MainCategoryFragment: Fragment(R.layout.fragment_main_category) {
+@AndroidEntryPoint
+class MainCategoryFragment : Fragment(R.layout.fragment_main_category) {
+
+    private lateinit var binding: FragmentMainCategoryBinding
+    private lateinit var specialProductsAdapter: SpecialProductsAdapter
+    private val viewModel by viewModels<MainCategoryViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainCategoryBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupSpecialProductsRv()
+
+        viewLifecycleOwner.lifecycleScope.launch(Dispatchers.Main) {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.specialProducts.collectLatest {
+                    when(it) {
+                        is Resource.Loading -> {
+                            showLoading()
+                        }
+                        is Resource.Success -> {
+                            Log.e("TAG", "Resource.Success")
+                            specialProductsAdapter.differ.submitList(it.data)
+                            hideLoading()
+                        }
+                        is Resource.Error -> {
+                            hideLoading()
+                            Log.e("TAG", it.message.toString())
+                            Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+                        }
+                        else -> Unit
+                    }
+                }
+            }
+        }
+    }
+
+    private fun showLoading() {
+        binding.mainCategoryProgressbar.visibility = View.VISIBLE
+    }
+
+    private fun hideLoading() {
+        binding.mainCategoryProgressbar.visibility = View.GONE
+    }
+
+    private fun setupSpecialProductsRv() {
+        specialProductsAdapter = SpecialProductsAdapter()
+        binding.rvSpecialProducts.apply {
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            adapter = specialProductsAdapter
+        }
+    }
 }
