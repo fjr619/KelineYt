05dff0fbd7cddf1bb140e1156876dd8210e83c4f
Franky <frank.jr.619@gmail.com>
2023-09-06 18:09:57 +0700

    base category setup

--- app/src/main/java/com/example/kelineyt/fragments/categories/BaseCategoryFragment.kt
+++ app/src/main/java/com/example/kelineyt/fragments/categories/BaseCategoryFragment.kt
@@ -1,7 +1,50 @@
 package com.example.kelineyt.fragments.categories
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kelineyt.R
+import com.example.kelineyt.adapters.BestProductsAdapter
+import com.example.kelineyt.databinding.FragmentBaseCategoryBinding
 
 open class BaseCategoryFragment: Fragment(R.layout.fragment_base_category) {
+    private lateinit var binding: FragmentBaseCategoryBinding
+    protected val offerAdapter: BestProductsAdapter by lazy { BestProductsAdapter() }
+    protected val  bestProductsAdapter: BestProductsAdapter by lazy { BestProductsAdapter() }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentBaseCategoryBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupOfferRv()
+        setupBestProductsRv()
+    }
+
+    private fun setupBestProductsRv() {
+        binding.rvBestProducts.apply {
+            layoutManager =
+                GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
+            adapter = bestProductsAdapter
+        }
+    }
+
+    private fun setupOfferRv() {
+        binding.rvOfferProducts.apply {
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            adapter = offerAdapter
+        }
+    }
 }
