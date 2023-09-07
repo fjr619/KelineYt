00f5475e75a0aecef6d09234df78942a089d2f4f
Franky <frank.jr.619@gmail.com>
2023-09-07 08:46:08 +0700

    category

--- app/src/main/java/com/example/kelineyt/fragments/categories/BaseCategoryFragment.kt
+++ app/src/main/java/com/example/kelineyt/fragments/categories/BaseCategoryFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.kelineyt.R
 import com.example.kelineyt.adapters.BestProductsAdapter
 import com.example.kelineyt.databinding.FragmentBaseCategoryBinding
@@ -30,6 +32,30 @@ open class BaseCategoryFragment: Fragment(R.layout.fragment_base_category) {
 
         setupOfferRv()
         setupBestProductsRv()
+
+        binding.rvOfferProducts.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                if (!recyclerView.canScrollHorizontally(1) && dx != 0){
+                    onOfferPagingRequest()
+                }
+            }
+        })
+
+        binding.nestedScrollBaseCategory.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener{ v, _, scrollY, _, _ ->
+            if (v.getChildAt(0).bottom <= v.height + scrollY){
+                onBestProductsPagingRequest()
+            }
+        })
+    }
+
+    open fun onOfferPagingRequest(){
+
+    }
+
+    open fun onBestProductsPagingRequest(){
+
     }
 
     private fun setupBestProductsRv() {
@@ -46,5 +72,21 @@ open class BaseCategoryFragment: Fragment(R.layout.fragment_base_category) {
                 LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
             adapter = offerAdapter
         }
+    }
+
+    fun showOfferLoading(){
+        binding.offerProductsProgressBar.visibility = View.VISIBLE
+    }
+
+    fun hideOfferLoading(){
+        binding.offerProductsProgressBar.visibility = View.GONE
+    }
+
+    fun showBestProductsLoading(){
+        binding.bestProductsProgressBar.visibility = View.VISIBLE
+    }
+
+    fun hideBestProductsLoading(){
+        binding.bestProductsProgressBar.visibility = View.GONE
     }
 }
--- app/src/main/java/com/example/kelineyt/fragments/categories/ChairFragment.kt
+++ app/src/main/java/com/example/kelineyt/fragments/categories/ChairFragment.kt
@@ -1,4 +1,78 @@
 package com.example.kelineyt.fragments.categories
 
-class ChairFragment: BaseCategoryFragment() {
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.example.kelineyt.data.Category
+import com.example.kelineyt.util.Resource
+import com.example.kelineyt.viewmodel.CategoryViewModel
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+import com.example.kelineyt.viewmodel.factory.BaseCategoryViewModelFactoryFactory
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class ChairFragment : BaseCategoryFragment() {
+
+    @Inject
+    lateinit var firestore: FirebaseFirestore
+
+    val viewModel by viewModels<CategoryViewModel> {
+        BaseCategoryViewModelFactoryFactory(firestore, Category.Chair)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewLifecycleOwner.lifecycleScope.launch(Dispatchers.Main) {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.offerProducts.collectLatest {
+                    when (it) {
+                        is Resource.Loading -> {
+                            showOfferLoading()
+                        }
+                        is Resource.Success -> {
+                            offerAdapter.differ.submitList(it.data)
+                            hideOfferLoading()
+                        }
+                        is Resource.Error -> {
+                            Snackbar.make(requireView(), it.message.toString(), Snackbar.LENGTH_LONG)
+                                .show()
+                            hideOfferLoading()
+                        }
+                        else -> Unit
+                    }
+                }
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch(Dispatchers.Main) {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.bestProducts.collectLatest {
+                    when (it) {
+                        is Resource.Loading -> {
+                            showBestProductsLoading()
+                        }
+                        is Resource.Success -> {
+                            bestProductsAdapter.differ.submitList(it.data)
+                            hideBestProductsLoading()
+                        }
+                        is Resource.Error -> {
+                            Snackbar.make(requireView(), it.message.toString(), Snackbar.LENGTH_LONG)
+                                .show()
+                            hideBestProductsLoading()
+                        }
+                        else -> Unit
+                    }
+                }
+            }
+        }
+    }
 }
